516cdbb83113b4f13e83b47eb47758c986726e61
Muri Budiman <muri.budiman@amscloud.co.id>
2026-02-11 15:14:14 +0700

    first commit

---
+++ app/Models/Rates.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Rates extends Model
+{
+    protected $fillable = [
+        'name',
+        'message',
+        'icon',
+        'color',
+        'action_url',
+        'is_read',
+        'is_system',
+        'category',
+    ];
+
+    public $timestamps = true;
+
+    public $incrementing = true;
+
+    protected $hidden = [];
+
+    protected $appends = [];
+}
---
+++ database/migrations/2026_02_11_080732_create_rates_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // Table Tarif Parkir
+        Schema::create('rates', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255)->unique()->comment('Nama Tarif Parkir');
+            $table->bigInteger('hourly_rate', 255)->comment('Tarif Parkir Per Jam');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rates');
+    }
+};
